ea7d6d08a2ef37047be122a64f32fdc80562087c
g0rbe <mail@danielgorbe.com>
2023-05-09 10:57:23 +0200

    update about text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,30 +2,70 @@
 	import { CodeBlock } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center max-w-[80%]">
-	<div class="text-center max-w-[50%]">
-		<h1 class="my-10 font-bold">About</h1>
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center">
+		<h1 class="font-bold">About</h1>
 
-		<hr class="my-10" />
+		<hr />
 
-		<h4 class="my-1">
+		<h4 class="max-sm:mx-6">
 			Columbus Project is a subdomain discovery service with an easy to integrate API.
 		</h4>
-		<p class="my-1">
+
+		<p class="max-sm:mx-6">
+			Columbus Project is a subdomain discovery service with an easy to integrate API.
+			<br />
 			An <a href="https://github.com/elmasy-com?q=columbus&type=all&language=&sort=">open source</a>
 			append only database of known subdomains. The goals are to discover, store and serve subdomains
 			as fast as possible with an easy to use API.
 		</p>
+
 		<img
-			class="max-w-screen-lg content-center invisible lg:visible"
+			class="mx-auto max-w-screen-lg max-lg:hidden lg:visible"
 			src="/gif/lookup.gif"
 			alt="Subdomain Lookup"
 		/>
 
-		<a
-			class="btn btn-l variant-ghost-primary my-9"
-			href="https://blog.elmasy.com/introducing-the-columbus-project/ ">Read more on the blog!</a
-		>
+		<h2 class="max-sm:mx-6">Sources</h2>
+
+		<h3 class="max-sm:mx-6">Certificate Transparency</h3>
+
+		<p class="max-sm:mx-6">
+			Multiple <a href="https://github.com/elmasy-com/columbus-scanner">scanner</a>
+			s continuously parsing the following logs:
+		</p>
+
+		<ul class="list sm:columns-2 md:columns-3">
+			<li>
+				<span class="flex-auto">Argon</span>
+			</li>
+			<li>
+				<span class="flex-auto"><s>Mammoth</s></span>
+			</li>
+			<li>
+				<span class="flex-auto">Nessie</span>
+			</li>
+			<li>
+				<span class="flex-auto">Nimbus</span>
+			</li>
+			<li>
+				<span class="flex-auto">Oak</span>
+			</li>
+			<li>
+				<span class="flex-auto">Sabre</span>
+			</li>
+			<li>
+				<span class="flex-auto">Trustasia</span>
+			</li>
+			<li>
+				<span class="flex-auto">Xenon</span>
+			</li>
+			<li>
+				<span class="flex-auto">Yeti</span>
+			</li>
+		</ul>
+
+		<a class="btn btn-l variant-ghost-primary my-10" href="/tools">Check the usefull tools!</a>
 	</div>
 </div>
 
