781709f1b365fe4fb80a7e9b89105feada79708b
g0rbe <mail@danielgorbe.com>
2023-05-13 14:20:39 +0200

    add a custom 404 page

---
+++ src/routes/404/+layout.svelte
@@ -0,0 +1,77 @@
+<script>
+	// Your custom Skeleton theme:
+	import '../../theme.postcss';
+
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/all.css';
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import '../../app.postcss';
+
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<svelte:head>
+	<title>About - Columbus Project</title>
+
+	<meta
+		name="description"
+		content="Columbus Project is an API first subdomain discovery service. A blazingly fast subdomain enumeration service with advanced features."
+	/>
+
+	<link rel="icon" href="/icon.svg" type="image/svg+xml" />
+
+	<link rel="canonical" href="https://columbus.elmasy.com/about" />
+
+	<meta property="og:site_name" content="About - Columbus Project" />
+
+	<meta property="og:type" content="website" />
+
+	<meta property="og:title" content="About - Columbus Project" />
+
+	<meta
+		property="og:description"
+		content="Columbus Project is an API first subdomain discovery service. A blazingly fast subdomain enumeration service with advanced features."
+	/>
+
+	<meta property="og:url" content="https://columbus.elmasy.com/about" />
+
+	<meta property="og:image" content="/cover.png" />
+
+	<meta property="article:published_time" content="2023-05-09T00:00:00.000Z" />
+
+	<meta property="article:modified_time" content="2023-05-10T00:00:00.000Z" />
+
+	<meta name="twitter:card" content="summary_large_image" />
+
+	<meta name="twitter:title" content="About - Columbus Project" />
+
+	<meta
+		name="twitter:description"
+		content="Columbus Project is an API first subdomain discovery service. A blazingly fast subdomain enumeration service with advanced features."
+	/>
+
+	<meta name="twitter:url" content="https://columbus.elmasy.com/about" />
+
+	<meta name="twitter:image" content="/cover.png" />
+
+	<meta name="twitter:label1" content="Written by" />
+
+	<meta name="twitter:data1" content="Daniel Gorbe" />
+
+	<meta name="twitter:label2" content="Filed under" />
+
+	<meta name="twitter:data2" content="Columbus - Elmasy" />
+
+	<meta property="og:image:width" content="1501" />
+
+	<meta property="og:image:height" content="501" />
+
+	<meta property="article:tag" content="Elmasy" />
+</svelte:head>
+
+<!-- App Shell -->
+<AppShell>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,10 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center">
+		<img class="max-w-[40%] inline" src="logo_white.svg" alt="Elmasy Logo" />
+		<h1 class="font-bold">Columbus Project</h1>
+		<hr />
+
+		<h2>Page Not Found!</h2>
+		<a class="btn btn-l variant-ghost-primary max-sm:mx-3 sm:mx-5" href="/">Go to the site!</a>
+	</div>
+</div>
